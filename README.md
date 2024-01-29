7ad027794821321893cbccd07da8368ca3150735
Alex Nazarenko <alexeybread81@gmail.com>
2024-01-29 16:18:13 +0000

    fix typo

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hunt the Wumpus
+
+(general description)
+
+## Run the program
+
+(instructions on how to run the program)
--- src/tests/CaveSystemTests.java
+++ src/tests/CaveSystemTests.java
@@ -9,60 +9,72 @@ import game.utils.Coordinates;
 
 public class CaveSystemTests {
 	@Test
+	public void basicCaveSystemTest() {
+		CaveSystem caves = new CaveSystem(20, 20);
+
+		caves.getCave(19, 19);
+
+		// cave.addUndirectedConnection(new Coordinates(1, 1), new Coordinates(2, 1));
+		// cave.addUndirectedConnection(new Coordinates(1, 1), new Coordinates(1, 0));
+
+		assertEquals("check connections string", "ru", caves.getConnectionsString(1, 1));
+	}
+
+	@Test
 	public void getConnectionsStringUpTest() {
-		CaveSystem cave = new CaveSystem(10, 10);
+		CaveSystem caves = new CaveSystem(10, 10);
 
-		cave.addUndirectedConnection(new Coordinates(1, 1), new Coordinates(1, 0));
+		caves.addUndirectedConnection(new Coordinates(1, 1), new Coordinates(1, 0));
 
-		assertEquals("check connections string", "u", cave.getConnectionsString(1, 1));
+		assertEquals("check connections string", "u", caves.getConnectionsString(1, 1));
 	}
 
 	@Test
 	public void getConnectionsStringDownTest() {
-		CaveSystem cave = new CaveSystem(10, 10);
+		CaveSystem caves = new CaveSystem(10, 10);
 
-		cave.addUndirectedConnection(new Coordinates(1, 1), new Coordinates(1, 2));
+		caves.addUndirectedConnection(new Coordinates(1, 1), new Coordinates(1, 2));
 
-		assertEquals("check connections string", "d", cave.getConnectionsString(1, 1));
+		assertEquals("check connections string", "d", caves.getConnectionsString(1, 1));
 	}
 
 	@Test
 	public void getConnectionsStringLeftTest() {
-		CaveSystem cave = new CaveSystem(10, 10);
+		CaveSystem caves = new CaveSystem(10, 10);
 
-		cave.addUndirectedConnection(new Coordinates(1, 1), new Coordinates(0, 1));
+		caves.addUndirectedConnection(new Coordinates(1, 1), new Coordinates(0, 1));
 
-		assertEquals("check connections string", "l", cave.getConnectionsString(1, 1));
+		assertEquals("check connections string", "l", caves.getConnectionsString(1, 1));
 	}
 
 	@Test
 	public void getConnectionsStringRightTest() {
-		CaveSystem cave = new CaveSystem(10, 10);
+		CaveSystem caves = new CaveSystem(10, 10);
 
-		cave.addUndirectedConnection(new Coordinates(1, 1), new Coordinates(2, 1));
+		caves.addUndirectedConnection(new Coordinates(1, 1), new Coordinates(2, 1));
 
-		assertEquals("check connections string", "r", cave.getConnectionsString(1, 1));
+		assertEquals("check connections string", "r", caves.getConnectionsString(1, 1));
 	}
 
 	@Test
 	public void getConnectionsStringFourTest() {
-		CaveSystem cave = new CaveSystem(10, 10);
+		CaveSystem caves = new CaveSystem(10, 10);
 
-		cave.addUndirectedConnection(new Coordinates(1, 1), new Coordinates(2, 1));
-		cave.addUndirectedConnection(new Coordinates(1, 1), new Coordinates(0, 1));
-		cave.addUndirectedConnection(new Coordinates(1, 1), new Coordinates(1, 2));
-		cave.addUndirectedConnection(new Coordinates(1, 1), new Coordinates(1, 0));
+		caves.addUndirectedConnection(new Coordinates(1, 1), new Coordinates(2, 1));
+		caves.addUndirectedConnection(new Coordinates(1, 1), new Coordinates(0, 1));
+		caves.addUndirectedConnection(new Coordinates(1, 1), new Coordinates(1, 2));
+		caves.addUndirectedConnection(new Coordinates(1, 1), new Coordinates(1, 0));
 
-		assertEquals("check connections string", "dlru", cave.getConnectionsString(1, 1));
+		assertEquals("check connections string", "dlru", caves.getConnectionsString(1, 1));
 	}
 
 	@Test
 	public void getConnectionsStringTwoTest() {
-		CaveSystem cave = new CaveSystem(10, 10);
+		CaveSystem caves = new CaveSystem(10, 10);
 
-		cave.addUndirectedConnection(new Coordinates(1, 1), new Coordinates(2, 1));
-		cave.addUndirectedConnection(new Coordinates(1, 1), new Coordinates(1, 0));
+		caves.addUndirectedConnection(new Coordinates(1, 1), new Coordinates(2, 1));
+		caves.addUndirectedConnection(new Coordinates(1, 1), new Coordinates(1, 0));
 
-		assertEquals("check connections string", "ru", cave.getConnectionsString(1, 1));
+		assertEquals("check connections string", "ru", caves.getConnectionsString(1, 1));
 	}
 }
