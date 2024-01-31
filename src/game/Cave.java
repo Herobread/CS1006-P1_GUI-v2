7ee6d93c7d94cc48fc5a58e3332350ab13b13ac7
Alex Nazarenko <alexeybread81@gmail.com>
2024-01-31 17:13:03 +0000

    remove isTunnel from cave

--- src/game/Cave.java
+++ src/game/Cave.java
@@ -3,14 +3,10 @@ package game;
 import java.util.ArrayList;
 
 public class Cave {
-	private boolean isTunnel;
 	private ArrayList<Entity> entities;
-	// is left
-	// right
 
-	public Cave(boolean isTunnel) {
+	public Cave() {
 		this.entities = new ArrayList<>();
-		this.isTunnel = isTunnel;
 	}
 
 	public void addEntity(Entity entity) {
@@ -19,13 +15,5 @@ public class Cave {
 
 	public ArrayList<Entity> getEntities() {
 		return entities;
-	}
-
-	public boolean isTunnel() {
-		return isTunnel;
-	}
-
-	public void setTunnel(boolean isTunnel) {
-		this.isTunnel = isTunnel;
 	}
 }
