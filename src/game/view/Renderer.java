f890f039bbcb28bf09a539bf759f654937d49163
= <=>
2024-02-04 17:30:34 +0000

    Manually merged other stuff

--- src/game/view/Renderer.java
+++ src/game/view/Renderer.java
@@ -3,6 +3,9 @@ package game.view;
 import java.awt.Image;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.awt.AlphaComposite;
+import java.awt.Graphics2D;
+import java.awt.Graphics;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
@@ -10,6 +13,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 import java.awt.Font;
 import java.awt.Color;
@@ -100,7 +104,42 @@ public class Renderer {
 			drawTexture("missing-texture", x, y, width, height);
 		}
 	}
+	public void drawTexture(String textureName, int x, int y, int width, int height, float opacity) {
+		String texturePath = "./resources/" + textureName + ".png";
+		try {
+			BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR);
+			image = ImageIO.read(new File(texturePath));
+			//AlphaComposite ac = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, opacity);
+			Image scaledImage = image.getScaledInstance(width, height, Image.SCALE_DEFAULT);
+			JLabel imageObject = new JLabel(new ImageIcon(scaledImage));
+			imageObject.setBounds(x, y, width, height);
+			frame.add(imageObject);
+			JPanel p = new JPanel();
+			Graphics g = frame.getGraphics();
+			paint(p,g, scaledImage,x,y,opacity);
+			frame.add(p);
+		} catch (IOException e) {
+			drawTexture("missing-texture", x, y, width, height);
+			try {
+				final BufferedImage image = ImageIO.read(new File(missingTexture));
+				drawTexture("missing-texture", x, y, image.getWidth(), image.getHeight());
+			} catch (IOException e2) {
+				System.out.println("Critical error in drawing texture");
+			}
+		}
+	}
+	public void paint(JPanel p, Graphics g, Image scaledImage, int x, int y,float opacity){
+		p.paint(g);
+		drawPicture(g, scaledImage, x,y,opacity);
+	}
 
+	public void drawPicture(Graphics g, Image pic, int x, int y,float opacity){
+		Graphics2D g2d = (Graphics2D) g;
+		AlphaComposite ac = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, opacity);
+		g2d.setComposite(ac);
+		g2d.drawImage(pic, x, y,null);
+
+	}
 	public void drawText(String text, int x, int y, int size) {
 		JLabel label = new JLabel(text);
 		Font font = new Font("DejaVu Sans Mono", Font.PLAIN, size);
