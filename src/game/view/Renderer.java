7408ca5fc677db3ff0c520a2763867cfbaa52746
Alex Nazarenko <alexeybread81@gmail.com>
2024-02-05 09:07:32 +0000

    remove raw texture files

--- src/game/view/Renderer.java
+++ src/game/view/Renderer.java
@@ -104,19 +104,21 @@ public class Renderer {
 			drawTexture("missing-texture", x, y, width, height);
 		}
 	}
+
 	public void drawTexture(String textureName, int x, int y, int width, int height, float opacity) {
 		String texturePath = "./resources/" + textureName + ".png";
 		try {
 			BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR);
 			image = ImageIO.read(new File(texturePath));
-			//AlphaComposite ac = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, opacity);
+			// AlphaComposite ac = AlphaComposite.getInstance(AlphaComposite.SRC_OVER,
+			// opacity);
 			Image scaledImage = image.getScaledInstance(width, height, Image.SCALE_DEFAULT);
 			JLabel imageObject = new JLabel(new ImageIcon(scaledImage));
 			imageObject.setBounds(x, y, width, height);
 			frame.add(imageObject);
 			JPanel p = new JPanel();
 			Graphics g = frame.getGraphics();
-			paint(p,g, scaledImage,x,y,opacity);
+			paint(p, g, scaledImage, x, y, opacity);
 			frame.add(p);
 		} catch (IOException e) {
 			drawTexture("missing-texture", x, y, width, height);
@@ -128,18 +130,20 @@ public class Renderer {
 			}
 		}
 	}
-	public void paint(JPanel p, Graphics g, Image scaledImage, int x, int y,float opacity){
+
+	public void paint(JPanel p, Graphics g, Image scaledImage, int x, int y, float opacity) {
 		p.paint(g);
-		drawPicture(g, scaledImage, x,y,opacity);
+		drawPicture(g, scaledImage, x, y, opacity);
 	}
 
-	public void drawPicture(Graphics g, Image pic, int x, int y,float opacity){
+	public void drawPicture(Graphics g, Image pic, int x, int y, float opacity) {
 		Graphics2D g2d = (Graphics2D) g;
 		AlphaComposite ac = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, opacity);
 		g2d.setComposite(ac);
-		g2d.drawImage(pic, x, y,null);
+		g2d.drawImage(pic, x, y, null);
 
 	}
+
 	public void drawText(String text, int x, int y, int size) {
 		JLabel label = new JLabel(text);
 		Font font = new Font("DejaVu Sans Mono", Font.PLAIN, size);
