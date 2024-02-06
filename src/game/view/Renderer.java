ec2d81c8f8585abaacdac8a5d8b7019e9413b4d4
JoshuaBeighton <joshualbeighton@gmail.com>
2024-02-06 16:31:31 +0000

    Added resilience to renderer

--- src/game/view/Renderer.java
+++ src/game/view/Renderer.java
@@ -1,10 +1,8 @@
 package game.view;
-
+//imports
 import java.awt.Image;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
-import java.awt.AlphaComposite;
-import java.awt.Graphics2D;
 import java.awt.Graphics;
 import java.io.File;
 import java.io.IOException;
@@ -14,11 +12,9 @@ import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
-import java.awt.Font;
-import java.awt.FontFormatException;
 import java.awt.Color;
-import java.awt.GraphicsEnvironment;;
 
+//extend JFrame
 public class Renderer extends JFrame{
 	// Singleton instance
 	private static Renderer instance;
@@ -28,8 +24,8 @@ public class Renderer extends JFrame{
 	// jframe is strange and has strange sizing
 	// maybe it includes border sizes
 	// as a result 512*512 turns into 528*548
-	private int width = 528;
-	private int height = 548;
+	private int width = 512;
+	private int height = 512;
 	private final String missingTexture = "./resources/missing-texture.png";
 
 	// Private constructor to prevent external instantiation
@@ -37,6 +33,7 @@ public class Renderer extends JFrame{
 		configureWindow();
 	}
 	
+	//configure attributes of the JFrame
 	private void configureWindow() {
 		setLayout(null);
 		setDimensions(width, height);
@@ -57,27 +54,32 @@ public class Renderer extends JFrame{
 		return instance;
 	}
 
+	//return the height of the frame
 	public int getFrameHeight() {
 		return height;
 	}
 		
+	//return the width of the frame
 	public int getFrameWidth() {
 		return width;
 	}
 
 	// Method to set width and height
 	public void setDimensions(int width, int height) {
-		this.width = width;
-		this.height = height;
+		this.width = width + getInsets().left + getInsets().right;
+		this.height = height + getInsets().top;
 		setSize(width, height);
 	}
 
 	// temporary method to handle clicks
 	public void drawButtonUnstable(String buttonText, int x, int y, ActionListener actionListener) {
-		JButton button = new JButton(buttonText);
-		button.setBounds(x, y, 150, 30);
-		button.addActionListener(actionListener);
-		add(button);
+		y+=getInsets().top;
+		if (validCoordinates(x, y)){
+			JButton button = new JButton(buttonText);
+			button.setBounds(x, y, 150, 30);
+			button.addActionListener(actionListener);
+			add(button);
+		}
 	}
 
 	public void draw() {
@@ -117,7 +119,6 @@ public class Renderer extends JFrame{
 				System.out.println("Critical error in drawing texture");
 			}
 		}
-
 	}
 
 	public void drawTexture(String textureName, int x, int y, int width, int height) {
@@ -125,20 +126,23 @@ public class Renderer extends JFrame{
 	}
 
 	public void drawTexture(String textureName, int x, int y, int width, int height, float opacity) {
-		String texturePath = "./resources/" + textureName + ".png";
-		try {
-			Image scaledImage = ImageIO.read(new File(texturePath)).getScaledInstance(width, height, Image.SCALE_DEFAULT);
-			TexturePanel t = new TexturePanel(scaledImage, x, y, width, height, opacity);
-			TexturePanels.add(t);
-			t.repaint();
-			add(t);
-		} catch (IOException e) {
-			drawTexture("missing-texture", x, y, width, height);
+		y+=getInsets().top;
+		if (validCoordinates(x, y)){
+			String texturePath = "./resources/" + textureName + ".png";
 			try {
-				final BufferedImage image = ImageIO.read(new File(missingTexture));
-				drawTexture("missing-texture", x, y, image.getWidth(), image.getHeight());
-			} catch (IOException e2) {
-				System.out.println("Critical error in drawing texture");
+				Image scaledImage = ImageIO.read(new File(texturePath)).getScaledInstance(width, height, Image.SCALE_DEFAULT);
+				TexturePanel t = new TexturePanel(scaledImage, x, y, width, height, opacity);
+				TexturePanels.add(t);
+				t.repaint();
+				add(t);
+			} catch (IOException e) {
+				drawTexture("missing-texture", x, y, width, height);
+				try {
+					final BufferedImage image = ImageIO.read(new File(missingTexture));
+					drawTexture("missing-texture", x, y, image.getWidth(), image.getHeight());
+				} catch (IOException e2) {
+					System.out.println("Critical error in drawing texture");
+				}
 			}
 		}
 	}
@@ -154,28 +158,16 @@ public class Renderer extends JFrame{
 		}
 	}
 
-	public void drawPicture(Graphics g, Image pic, int x, int y, float opacity) {
-		Graphics2D g2d = (Graphics2D) g;
-		AlphaComposite ac = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, opacity);
-		g2d.setComposite(ac);
-		g2d.drawImage(pic, x, y, null);
-	}
 
 	public void drawText(String text, int x, int y, int size, Color colour) {
-		TextPanel t = new TextPanel(text, x, y, x, y, size, colour);
-		add(t);
-		TextPanels.add(t);
+		y+=getInsets().top*2;
+		if (validCoordinates(x, y)){
+			TextPanel t = new TextPanel(text, x, y, x, y, size, colour);
+			add(t);
+			TextPanels.add(t);
+		}
 	}
-	public Font getFont(int size) {
-        try {
-            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("./resources/font.ttf")));
-            return new Font("Pixelify Sans Regular", Font.TRUETYPE_FONT, size);
-        } catch (IOException | FontFormatException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-        return new Font("Consolas", Font.TRUETYPE_FONT, size);
-    }
+
 	public void clear() {
 		getContentPane().removeAll();
 		TexturePanels.clear();
@@ -185,5 +177,16 @@ public class Renderer extends JFrame{
 	// Ensure the Singleton pattern by adding a private readObject method
 	private Object readResolve() {
 		return instance;
+	}
+	private boolean validCoordinates(int x, int y){
+		if (x<0 || y< 0){
+			System.err.println("Drawing starts at negative coordinates!");
+			return false;
+		}
+		if (x>width || y>height){
+			System.err.println("Drawing starts outside frame!");
+			return false;
+		}
+		return true;
 	}
 }
