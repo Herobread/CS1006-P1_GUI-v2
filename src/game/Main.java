bc365669a8da7e137ff54e671597b3e97dde9098
Alex Nazarenko <alexeybread81@gmail.com>
2024-02-08 16:15:33 +0000

    move game initialization

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,30 +1,13 @@
 package game;
 
-import game.controllers.dialogue.DialogueManager;
-import game.controllers.game.GameStateManager;
+import game.controllers.game.logic.initialization.InitializeGame;
 import game.controllers.view.ViewManager;
-import game.model.CaveSystem;
-import game.model.WorldGenerator;
-import game.model.entities.Player;
 
 class Main {
 	public static void main(String[] args) {
+		InitializeGame.initialize();
+
 		ViewManager viewManager = ViewManager.getInstance();
-		GameStateManager gameStateManager = GameStateManager.getInstance();
-
-		// initialise game variables
-		Player player = new Player(0, 0);
-		gameStateManager.setPlayer(player);
-
-		CaveSystem caves = new CaveSystem(20, 20);
-		WorldGenerator generator = new WorldGenerator(20, 20);
-		generator.generateCellularAutomataCaves(caves);
-
-		gameStateManager.setCaves(caves);
-
-		// viewManager.switchToMap();
 		viewManager.switchToMainMenu();
-		// viewManager.switchToDialogue();
-		// viewManager.switchToGameplay();
 	}
 }
